e6b398ce8b8c4830aa73650fdb8f5a32bc32eaac
Tatchai Samutsopakul <iridescentz67@gmail.com>
2025-10-06 11:26:16 +0700

    fix yq

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def details():
         {
             "time": datetime.today().strftime("%I:%M:%S %p on %B %d, %Y"),
             "hostname": socket.gethostname(),
-            "message": "Hello, World!!",
+            "message": "Hello, World!",
         }
     )
 
